6263fa960c5c8a3654521121f0e755e7eb1ff6fe
Pedro Calado Moura <pcaladomoura@gmail.com>
2022-08-17 19:44:31 -0300

    add build.js and images.json

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
 
+
+
+const randomImg = (path = '/images/')=>{
+    
+    
+    console.log(files)
+    console.log(files[Math.floor(Math.random() * files.length)] )
+    return 
+}
+
 app.get("/",(req,res) =>{
     console.log(req.query)
+    randomImg()
     res.sendFile(__dirname + "\\Images\\AI\\tsunade artificial.png")
 });
+
+
 app.listen(3000);
---
+++ build.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+const files =fs.readdirSync(__dirname + '/images/');
+let data = {};
+files.forEach((folder)=>{
+    data[folder] = fs.readdirSync(__dirname + '/images/' + folder);
+})
+
+
+
+fs.writeFile(__dirname + '/images.json',JSON.stringify(data), (err) =>{if(err) throw err; else console.log("Json file created");}); 
