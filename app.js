9ae7391c66cb2a8043cb5a8e82913933f76fc0e2
Pedro Calado Moura <pcaladomoura@gmail.com>
2022-08-17 20:19:00 -0300

    add ramdom img

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const fs = require("fs");
+const ImagesNames = JSON.parse(fs.readFileSync("images.json"));
 
-
-
-const randomImg = (path = '/images/')=>{
-    
-    
-    console.log(files)
-    console.log(files[Math.floor(Math.random() * files.length)] )
-    return 
-}
-
-app.get("/",(req,res) =>{
-    console.log(req.query)
-    randomImg()
-    res.sendFile(__dirname + "\\Images\\AI\\tsunade artificial.png")
+const randomImg = (path) => {
+  const keys = Object.keys(ImagesNames);
+  const folder = path || keys[Math.floor(keys.length * Math.random())];
+  const listfolder = ImagesNames[folder];
+  return `${folder}\\${
+    listfolder[Math.floor(Math.random() * listfolder.length)]
+  }`;
+};
+app.get("/", (req, res) => {
+  console.log(req.query);
+  res.sendFile(__dirname + "\\Images\\" + randomImg(req.query.filter));
 });
-
 
 app.listen(3000);
--- build.js
+++ build.js
@@ -1,11 +1,12 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const files =fs.readdirSync(__dirname + '/images/');
+const files = fs.readdirSync(__dirname + "/images/");
 let data = {};
-files.forEach((folder)=>{
-    data[folder] = fs.readdirSync(__dirname + '/images/' + folder);
-})
+files.forEach((folder) => {
+  data[folder] = fs.readdirSync(__dirname + "/images/" + folder);
+});
 
-
-
-fs.writeFile(__dirname + '/images.json',JSON.stringify(data), (err) =>{if(err) throw err; else console.log("Json file created");}); 
+fs.writeFile(__dirname + "/images.json", JSON.stringify(data), (err) => {
+  if (err) throw err;
+  else console.log("Json file created");
+});
